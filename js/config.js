8b59ed51ad38072278e550f49e2afa5d10dfc682
angelsalazarmottola <42794630+angelsalazarmottola@users.noreply.github.com>
2025-01-14 21:59:38 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_38bMLVsP1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7n2lriq0j7dl3j1rs9idcl2dba', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6jt0mapbsqp8uiruho8uiu0ia5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
